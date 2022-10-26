726a33027caa49437ad1e28f1929e4798defef0e
AlexandreJusten <ajustenf@hotmail.com>
2022-10-25 21:35:01 -0300

    first Master

---
+++ cliente/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Inicial from './pages/inicial/index.jsx'
+
+function App() {
+ 
+
+  return (
+    <div className="App">
+      <Inicial/>
+    
+    </div>
+  )
+}
+
+export default App
